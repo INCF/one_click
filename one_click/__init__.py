d9f47aa4017a33b9a00183dfaf3af0aeea0d03a5
Christian Haselgrove <christian.haselgrove@umassmed.edu>
2012-01-27 15:45:01 +0100

    changed SMTP server

--- one_click/__init__.py
+++ one_click/__init__.py
@@ -17,7 +17,7 @@ import dicom
 
 auth = open('/home/ch/.xnat_pw').read().strip().encode('base64').strip()
 host = 'xnat.incf.org'
-mail_host = 'luna.incf.ki.se'
+mail_host = 'smtp.incf.org'
 admin_email = 'xnat-admin@incf.org'
 deleted_dir = '/data/cache/DELETED'
 
